c559c0c5fabe850188b37f981fae37dc460c7e4e
Zerubabel <zerucodes@gmail.com>
2024-02-01 00:46:18 +0300

    row added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Headers/Header";
+import RowList from "../../components/Rows/RowList/RowList";
 
 const Home = () => {
   return (
     <>
       <Header />
+
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
